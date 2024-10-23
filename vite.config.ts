b99d2f626caa75c37ee68c2bbecd27ee27d0472c
znycheporuk <nycheporuk.zakhar@gmail.com>
2024-10-23 22:12:10 +0300

    split bundle

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,22 @@ export default defineConfig({
 	build: {
 		target: "esnext",
 		sourcemap: true,
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					if (id.includes("node_modules")) {
+						const packagePath = id.split("node_modules/")[1].split("/");
+						const isOrg = packagePath[0].startsWith("@");
+						const packageName = isOrg
+							? `${packagePath[0]}-${packagePath[1]}`
+							: packagePath[0];
+						return packageName;
+					}
+					// if (id.includes("/src/")) {
+					// 	return id.split("/").at(-1)?.split(".")[0];
+					// }
+				},
+			},
+		},
 	},
 });
